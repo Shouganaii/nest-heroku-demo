94bf19a772b34e2648f3cfcc0d8de32633fb47df
Shouganaii <frias.caio@yahoo.com.br>
2020-09-24 22:54:43 -0300

    teste

---
+++ src/app.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+import { PRODUCTS } from './mock/products.mock';
+@Injectable()
+export class AppService {
+  products = PRODUCTS;
+  getHello(): string {
+    return 'Hello World!';
+  }
+  async getProducts() {
+    return await this.products;
+  }
+}
